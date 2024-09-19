860d1d1a80b96e43f2e1ae9c50a4f3035ae0d9c8
Dirgam123 <dergemfeheleve@gmail.com>
2024-09-20 03:21:12 +0700

    aaaaaaaaa

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-// Function to handle login
 function login(event) {
-  event.preventDefault(); // Prevent form from submitting
+  event.preventDefault();
 
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  // Basic login validation (for example purposes)
   if (username === "admin" && password === "admin") {
-    window.location.href = "dashboard.html"; // Redirect to dashboard
+    window.location.href = "dashboard.html";
   } else {
     document.getElementById("error-message").innerText =
       "Invalid username or password";
   }
 }
 
-// Function to handle logout (optional)
 function logout() {
   alert("You have logged out!");
-  window.location.href = "index.html"; // Redirect to login page
+  window.location.href = "index.html";
 }
