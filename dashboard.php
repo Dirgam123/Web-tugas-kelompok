560ccfcfc2be2ffb531b6572af22e6dbdb8e3c43
Dirgam123 <dergemfeheleve@gmail.com>
2024-10-12 16:19:19 +0700

    update navbar

--- dashboard.php
+++ dashboard.php
@@ -13,11 +13,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Smart City</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
-<body>
-    <?php include "layout/header.html" ?>
+<body class="container">
+    <?php include "layout/headerlogin.html" ?>
     <h3>Selamat Datang di Smart City, <?= $_SESSION["username"] ?></h3>
+    
     <form action="dashboard.php" method="POST">
         <button type="submit" name="logout">logout</button>
     </form>
